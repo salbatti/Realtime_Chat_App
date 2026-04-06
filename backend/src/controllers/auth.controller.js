1ef203589002f2af465a5f880f225d123b66a7dd
Steve George <98937566+salbatti@users.noreply.github.com>
2026-04-06 19:13:13 +0530

    Welcome email implemented at SignUp

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
-
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import { ENV } from "../lib/env.js";
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body
     try {
@@ -45,6 +46,14 @@ export const signup = async (req, res) => {
             // } catch (error) {
             //     console.error("Failed to send welcome email:", error);
             // }
+
+            //todo:send a welcome email to the user after successful registration
+
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.log("Failed to send welcome email",error);
+            }
         }
         else {
             res.status(400).json({ message: "Invalid issue" })

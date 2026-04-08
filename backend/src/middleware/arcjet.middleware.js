e33c8cf33be41cf4b3445d1474ebaf672b5c1412
Steve George <98937566+salbatti@users.noreply.github.com>
2026-04-08 12:18:13 +0530

    arcject-middleware

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -3,6 +3,7 @@ import aj from "../lib/arcjet.js";
 import { isSpoofedBot } from "@arcjet/inspect";
 
 
+
 export const arcjetProtection = async (req, res, next) => {
     try {
         const decision = await aj.protect(req);

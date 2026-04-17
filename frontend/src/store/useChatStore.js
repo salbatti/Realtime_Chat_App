81db0009c93908f82bc3552603e596458ae32ba1
Steve George <98937566+salbatti@users.noreply.github.com>
2026-04-17 22:15:30 +0530

    chat-container-setup-done

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -23,29 +23,40 @@ export const useChatStore = create((set, get) => ({
     setSelectedUser: (selectedUser) => set({ selectedUser }),
 
 
-    getAllContacts:async()=>{
-        set({isUsersLoading:true})
+    getAllContacts: async () => {
+        set({ isUsersLoading: true })
         try {
-            const res=await axiosInstance.get("/messages/contacts");
-            set({allContacts:res.data})
+            const res = await axiosInstance.get("/messages/contacts");
+            set({ allContacts: res.data })
         } catch (error) {
             toast.error(error.response.data.message);
         }
-        finally{
-            set({isUsersLoading:false})
+        finally {
+            set({ isUsersLoading: false })
         }
     },
 
-    getMyChatPartners: async()=>{
-        set({isMessagesLoading:true})//issue here
+    getMyChatPartners: async () => {
+        set({ isUsersLoading: true })//issue here
         try {
-            const res =await axiosInstance.get("/messages/chats");
-            set({chats:res.data})
+            const res = await axiosInstance.get("/messages/chats");
+            set({ chats: res.data })
         } catch (error) {
             toast.error(error.response.data.message);
         }
-        finally{
-            set({isMessagesLoading:false})
+        finally {
+            set({ isUsersLoading: false })
         }
-    }
+    },
+    getMessagesByUserId: async (userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data });
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Something went wrong");
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+    },
 }))
